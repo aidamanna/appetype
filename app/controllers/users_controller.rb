688f75e2cdd8291f291fc727ce876599135b2d9d
aidamanna <aidamanna@gmail.com>
2017-09-03 14:11:49 +0200

    Create user session when signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to menus_path
     else
       render '/users/new'
