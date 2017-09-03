ab131c4409f4da9b31ad26a64d1aa0aa9d52def7
aidamanna <aidamanna@gmail.com>
2017-09-03 18:46:25 +0200

    Remove user name uniqueness restriction

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
 
   validates :name,
             presence: true,
-            uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
 
   validates :email,
