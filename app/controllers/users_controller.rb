417c9b6e2a81c72aca21c895cd09f753ff98f70e
aidamanna <aidamanna@gmail.com>
2017-09-12 23:31:19 +0200

    Deny access to users list if user not admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5).order(:name)
+    authorize! :index, @users
   end
 
   def new
