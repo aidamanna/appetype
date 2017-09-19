e4e7f74b24903e280be9901d212ed6451d59417d
aidamanna <aidamanna@gmail.com>
2017-09-19 20:43:54 +0200

    Helper for retrieving the form

---
+++ app/helpers/retrieve_form.rb
@@ -0,0 +1,15 @@
+class RetrieveForm
+  attr_reader :form_definition
+
+  def initialize(form_id)
+    response = RestClient.get("#{Config.forms_endpoint}/#{form_id}")
+
+    @form_definition = JSON.parse(response.body, symbolize_names: true)
+  rescue RestClient::Exception => err
+    puts "Error retrieving form with id: #{form_id} " \
+         "Response code: #{err.http_code} " \
+         "Response body: #{err.http_body}"
+
+    raise 'The form cannot be created'
+  end
+end
