48b2ede94982bb3d427af9e0385af93b0d904f7d
aidamanna <aidamanna@gmail.com>
2017-09-20 23:42:08 +0200

    Fix rubocop offense: Delimit %i literals by ( and )

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,13 +7,13 @@ class Ability
     can :manage, :all if user.role?(:admin)
 
     if user.role?(:catering)
-      can %i[read create update], Menu
-      can %i[show update], User, id: user.id
+      can %i(read create update), Menu
+      can %i(show update), User, id: user.id
     end
 
     if user.role?(:diner)
-      can %i[read fill], Menu
-      can %i[show update], User, id: user.id
+      can %i(read fill), Menu
+      can %i(show update), User, id: user.id
     end
   end
 end
