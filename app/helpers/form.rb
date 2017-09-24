1e408c7ca7e94a8d2e9240d16cabe65294411ff2
aidamanna <aidamanna@gmail.com>
2017-09-24 12:28:11 +0200

    Fix rubocop offense: Shadowing outer local variable

--- app/helpers/form.rb
+++ app/helpers/form.rb
@@ -1,5 +1,4 @@
 class Form
-
   def initialize(form_definition)
     @form_definition = form_definition
   end
@@ -14,14 +13,14 @@ class Form
   private
 
   def multiple_choice_with_id(field_id)
-    field = @form_definition[:fields].find {|field| field[:id] == field_id && field[:type] == 'multiple_choice'}
+    field = @form_definition[:fields].find { |f| f[:id] == field_id && f[:type] == 'multiple_choice' }
 
     raise "No field with id: #{field_id}" if field.nil?
     field
   end
 
   def choice_with_label(choice_label, field)
-    choice = field[:properties][:choices].find {|choice| choice[:label] == choice_label}
+    choice = field[:properties][:choices].find { |c| c[:label] == choice_label }
 
     raise "No choice with label: #{choice_label}" if choice.nil?
     choice
