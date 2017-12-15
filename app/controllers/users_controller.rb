7a08bee0f0cfb03d24ddce6f5566dea536b84cf0
Aida Manna <aida@typeform.com>
2017-12-15 17:00:45 +0100

    Fix rubocop offenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      flash[:success] = "User updated"
+      flash[:success] = 'User updated'
       redirect_to user_path(@user)
     else
       render 'edit'
