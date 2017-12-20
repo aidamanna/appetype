daaf5c419786d66abaa0ec62dee09e1ffc9eb1a6
Aida Manna <aida@typeform.com>
2017-12-20 18:56:11 +0100

    Add tests for oauth client

---
+++ spec/api_clients/oauth_client_spec.rb
@@ -0,0 +1,26 @@
+describe OauthClient do
+  describe '#retrieve_token' do
+    it 'hits typeform api with the correct parameters' do
+      url = Config.typeform_base_endpoint + '/oauth/token'
+      expected_oauth_token = 'aBCdE'
+
+      stub_request(:post, url).to_return(
+        body: {
+          access_token: expected_oauth_token
+        }.to_json
+      )
+
+      temp_auth_code = 'XyZ'
+      oauth_token = OauthClient.new.retrieve_token(temp_auth_code)
+      expect(oauth_token).to eql(expected_oauth_token)
+
+      expect(WebMock).to have_requested(:post, url)
+        .with(body: URI.encode_www_form(
+          'code' => temp_auth_code,
+          'client_id' => Config.typeform_client_id,
+          'client_secret' => Config.typeform_client_secret,
+          'redirect_uri' => Config.base_url + '/oauth/token'
+        ))
+    end
+  end
+end
