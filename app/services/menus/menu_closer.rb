1b3a3db89707a123c4222cad2182e70ef1b3226c
aidamanna <aidamanna@gmail.com>
2017-12-26 13:26:13 +0100

    Use local variables instead of instance variables

--- app/services/menus/menu_closer.rb
+++ app/services/menus/menu_closer.rb
@@ -1,9 +1,9 @@
 class MenuCloser
   def call(user_id, menu_id)
-    @menu = Menu.find(menu_id)
-    @menu.state = 'closed'
+    menu = Menu.find(menu_id)
+    menu.state = 'closed'
     oauth_token = OauthTokenRetriever.new.call(user_id)
-    FormClient.new(oauth_token).update(@menu.form, @menu.to_form_payload)
-    @menu.save
+    FormClient.new(oauth_token).update(menu.form, menu.to_form_payload)
+    menu.save
   end
 end
