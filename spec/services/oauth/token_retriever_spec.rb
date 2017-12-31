01b76b8e63a6f1037c022b4428ab1e3b19d9adb3
aidamanna <aidamanna@gmail.com>
2017-12-31 12:14:09 +0100

    Encapsulate test steps in methods for oauth token retriever service

--- spec/services/oauth/token_retriever_spec.rb
+++ spec/services/oauth/token_retriever_spec.rb
@@ -1,10 +1,17 @@
 describe Oauth::TokenRetriever do
   describe '.call' do
     it 'retrieves the oauth token' do
+      then_it_returns_then_oauth_token
+      when_asking_for_the_users_oauth_token
+    end
+
+    def then_it_returns_then_oauth_token
       token = double(:token)
       allow(Token).to receive(:find_by_user_id).and_return(token)
       expect(token).to receive(:[])
+    end
 
+    def when_asking_for_the_users_oauth_token
       Oauth::TokenRetriever.call(1)
     end
   end
