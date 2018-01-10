3eff35c62c73e16d2859c3c9d44dabac35adfad4
Aida Manna <aida@typeform.com>
2018-01-10 12:41:05 +0100

    Fix rubicon offenses

--- app/controllers/users/create_invite_controller.rb
+++ app/controllers/users/create_invite_controller.rb
@@ -3,26 +3,24 @@ module Users
     authorize_resource class: CreateInviteController
 
     def call
-      begin
-        email = params[:user_invitation][:email]
-        Users::InviteCreator.new.call(email)
+      email = params[:user_invitation][:email]
+      Users::InviteCreator.new.call(email)
 
-        flash[:success] = 'The invitation email has been sent'
-        redirect_to users_path
-      rescue Error::ExistingUser
-        flash[:danger] = 'This email address is already associated with an account'
+      flash[:success] = 'The invitation email has been sent'
+      redirect_to users_path
+    rescue Error::ExistingUser
+      flash[:danger] = 'This email address is already associated with an account'
 
-        @user_invitation = UserInvitation.new_with_email(email)
-        render 'users/invite'
-      rescue Error::DatabaseValidations => err
-        @validation_errors = err.errors
-        @user_invitation = UserInvitation.new_with_email(email)
-        render 'users/invite'
-      rescue => err
-        puts "Error inviting the user. Error: #{err}"
-        flash[:danger] = 'Error inviting the user'
-        redirect_to users_path
-      end
+      @user_invitation = UserInvitation.new_with_email(email)
+      render 'users/invite'
+    rescue Error::DatabaseValidations => err
+      @validation_errors = err.errors
+      @user_invitation = UserInvitation.new_with_email(email)
+      render 'users/invite'
+    rescue => err
+      puts "Error inviting the user. Error: #{err}"
+      flash[:danger] = 'Error inviting the user'
+      redirect_to users_path
     end
   end
 end
