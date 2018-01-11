d7dc5a19f552182d8ab59e4851d5d08a73f7d6a5
aidamanna <aidamanna@gmail.com>
2018-01-11 21:16:36 +0100

    Fix rubocop offenses

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def invite(user_invitation)
     @url = signup_url + "?token=#{user_invitation.token}"
-    attachments.inline["logo.png"] = File.read("#{Rails.root}/public/images/logo.png")
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/public/images/logo.png")
     mail(to: user_invitation.email, subject: 'You have been invited to use Appetype!')
   end
 end
