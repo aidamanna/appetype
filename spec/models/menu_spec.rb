afcf8564f7da46d9b196c5f33b0e9170c3e55a01
Aida Manna <aida@typeform.com>
2018-01-18 13:57:56 +0100

    Add tests for menu model

---
+++ spec/models/menu_spec.rb
@@ -0,0 +1,20 @@
+describe Menu do
+  subject { described_class.new(week: '2018W04') }
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a week' do
+      subject.week = nil
+      expect(subject).not_to be_valid
+    end
+  end
+
+  describe '.week_description' do
+    it 'provides the week description' do
+      expect(subject.week_description).to eql('January 22 to January 26')
+    end
+  end
+end
