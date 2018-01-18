b737c30ba2c4bdd8a4b94fd1d8e5594ed28455ff
aidamanna <aidamanna@gmail.com>
2018-01-18 11:49:42 +0100

    Do not fix rubocop offenses in db/schema so that travis does not detect not commited changes after running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,47 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_171_231_163_238) do
+ActiveRecord::Schema.define(version: 20171231163238) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'menus', force: :cascade do |t|
-    t.string   'week'
-    t.json     'daily_menus'
-    t.string   'state'
-    t.string   'form'
-    t.datetime 'created_at',  null: false
-    t.datetime 'updated_at',  null: false
+  create_table "menus", force: :cascade do |t|
+    t.string   "week"
+    t.json     "daily_menus"
+    t.string   "state"
+    t.string   "form"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table 'orders', force: :cascade do |t|
-    t.integer 'menu_id'
-    t.integer 'user_id'
-    t.json    'daily_orders'
+  create_table "orders", force: :cascade do |t|
+    t.integer "menu_id"
+    t.integer "user_id"
+    t.json    "daily_orders"
   end
 
-  create_table 'tokens', primary_key: 'user_id', id: :integer, force: :cascade do |t|
-    t.string 'oauth_token'
+  create_table "tokens", primary_key: "user_id", id: :integer, force: :cascade do |t|
+    t.string "oauth_token"
   end
 
-  create_table 'user_invitations', primary_key: 'token', id: :string, force: :cascade do |t|
-    t.string   'email'
-    t.datetime 'accepted_at'
-    t.datetime 'created_at', null: false
+  create_table "user_invitations", primary_key: "token", id: :string, force: :cascade do |t|
+    t.string   "email"
+    t.datetime "accepted_at"
+    t.datetime "created_at",  null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'name'
-    t.string   'email'
-    t.datetime 'created_at',                        null: false
-    t.datetime 'updated_at',                        null: false
-    t.string   'password_digest'
-    t.string   'role', default: 'diner'
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "password_digest"
+    t.string   "role",            default: "diner"
   end
 
-  create_table 'webhooks', force: :cascade do |t|
-    t.string 'event_id'
-    t.string 'event_type'
-    t.json   'form_response'
+  create_table "webhooks", force: :cascade do |t|
+    t.string "event_id"
+    t.string "event_type"
+    t.json   "form_response"
   end
+
 end
